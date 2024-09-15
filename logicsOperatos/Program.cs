9723fb1dd699e4a59aa1c7dca96055cb9dba985b
LanaSteen <l4n.steen@gmail.com>
2024-09-15 21:28:24 +0400

    Add project files.

---
+++ logicsOperatos/Program.cs
@@ -0,0 +1,47 @@
+﻿class Lesson2
+{
+    static void Main(string[] args)
+    {
+        int num1 = 5;
+        Console.WriteLine(num1++);
+        Console.WriteLine(num1);
+
+
+        switch (num1)
+        {
+            case int x when x == 1 && x == 4:
+                Console.WriteLine(x);
+                break;
+            case 3:
+                Console.WriteLine("samia");
+                break;
+
+            case 5:
+                Console.WriteLine("metia");
+                break;
+            default:
+                Console.WriteLine("udris");
+                break;
+        }
+
+        switch (num1)
+        {
+            case int x when x == 1 && x == 4:
+                Console.WriteLine(x);
+                break;
+            case 3:
+                Console.WriteLine("samia");
+                break;
+
+            case 5:
+                Console.WriteLine("metia");
+                break;
+            default:
+                Console.WriteLine("udris");
+                break;
+        }
+
+
+
+    }
+}
